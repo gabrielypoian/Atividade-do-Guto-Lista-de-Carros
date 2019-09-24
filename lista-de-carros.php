2581a0c15fbfca0367494c9248b899e033ca8df3
Gabriely Poian <42159614+gabrielypoian@users.noreply.github.com>
2019-09-23 22:10:10 -0300

    Add files via upload

---
+++ lista-de-carros.php
@@ -0,0 +1,7 @@
+<?php
+
+$carros = array("Placa: " => " LMS-2312", "Marca: " => "Volkswagen", "Modelo: " => "Golf");
+
+echo "Placa do carro: " .$carros["Placa: "]."<br>";
+echo "Placa do carro: " .$carros["Marca: "]."<br>";
+echo "Placa do carro: " .$carros["Modelo: "]."<br>";
